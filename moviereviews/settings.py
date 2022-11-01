e05954b17219d0f2b4b78e6f52884bf93ef6884f
jmmunoza <jmmunoza@eafit.edu.co>
2022-11-01 09:27:02 -0500

    IP

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-+=1@ghq*9m6c_m19x+1$=nsp&mz)fm_)qlaoy2ym+fob@&ycct
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['34.170.179.19']
 
 
 # Application definition
